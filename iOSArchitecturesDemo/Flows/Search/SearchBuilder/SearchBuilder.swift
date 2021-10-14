df09e119266638f853cb372412dbe9d9e8e5718d
Emil <kurbanov_emil@icloud.com>
2021-10-14 14:43:49 +0300

    Lesson7

---
+++ iOSArchitecturesDemo/Flows/Search/SearchBuilder/SearchBuilder.swift
@@ -0,0 +1,19 @@
+//
+//  SearchBuilder.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Veaceslav Chirita on 08.10.2021.
+//  Copyright © 2021 ekireev. All rights reserved.
+//
+
+import UIKit
+
+class SearchBuilder {
+    static func build() -> (UIViewController & SearchViewInput) {
+        let presenter = SearchPresenter()
+        let viewController = SearchViewController(presenter: presenter)
+        presenter.viewInput = viewController
+        
+        return viewController
+    }
+}
